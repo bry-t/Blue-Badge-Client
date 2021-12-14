2e0dfe056785679004c903ebbd2c5109a7ade394
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2021-12-14 06:56:47 -0800

    changes

--- src/Components/Meals/Meals.jsx
+++ src/Components/Meals/Meals.jsx
@@ -129,9 +129,13 @@ const Meals = (props) => {
 
 export default Meals;
 
-.then(data => {
-    for(let i = 0; i > data.foods; i++) {
-        const foodNutro = data.foods[i].foodNutrition
-    }
-    console.log(foodNutro)
-})
+// .then(data => {
+//     for(let i = 0; i > data.foods.length; i++) {
+//         const foodNutro = data.foods[i].foodNutrition;
+//         for(let j = 0; j > foodNutro.length; i++) {
+//             const nutrientId = foodNutro[j].nutrientId;
+            
+//         }
+//     }
+//     console.log(foodNutro)
+// })
