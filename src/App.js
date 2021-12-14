6da47224958f2878aa1e7fc340cfc9d5249b7b28
Andrew Broersma <broersma.andrew@gmail.com>
2021-12-14 09:19:33 -0500

    working login stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Meals from './Components/Meals/Meals';
 import Auth from './Components/Auth/Auth';
-
 
 function App() {
 
+  const [ sessionToken, setSessionToken ] = useState(undefined)
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"))
+    }
+  })
+
+  const updateLocalStorage = newToken => {
+    localStorage.setItem("token", newToken)
+    setSessionToken(newToken)
+  }
+
+  const clearLocalStorage = () => {
+    localStorage.clear()
+    setSessionToken(undefined)
+  }
+
+  const viewer = () => {
+    return sessionToken !== undefined ? 
+    <Navbar sessionToken={updateLocalStorage} clearSession={clearLocalStorage} /> &&
+    <Meals sessionToken={sessionToken}/> : 
+    <Auth updateLocalStorage={updateLocalStorage} />
+  }
+
   return (
-    <div >
-      <Auth />
+    <div>
+      {viewer()}
     </div>
   );
 }
--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import './auth.css';
 import { Form, FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.css";
-import RegisterModal from './RegisterModal'
+import ReactForm from "./RegisterForm";
 
 const Auth = (props) => {
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [register, setRegister] = useState(false);
+
+    const [show, setShow] = useState(false);
+
 
     const loginSubmit = (event) => {
         event.preventDefault()
@@ -21,8 +21,9 @@ const Auth = (props) => {
         }
 
         let url = "http://localhost:3333/user/login"
+        console.log(reqBody)
 
-            fetch(url, {
+        fetch(url, {
             method: "POST",
             body: JSON.stringify(reqBody),
             headers: new Headers({
@@ -34,150 +35,73 @@ const Auth = (props) => {
             .catch(err => console.log(err))
     }
 
+    const handleShow = () => setShow(true);
 
-    const registerSubmit = (event) => {
-        event.preventDefault()
-
-        let reqBody = {
-            firstName,
-            lastName,
-            email,
-            password
-        }
-
-        let url = "http://localhost:3333/user/register"
-
-        fetch(url, {
-            method: "POST",
-            body: JSON.stringify(reqBody),
-            headers: new Headers({
-                "Content-Type": "application/json"
-            })
-        })
-            .then(res => res.json())
-            .then(data => {
-                props.updateLocalStorage(data.token);
-                setRegister(false)
-            })
-            .catch(err => console.log(err))
-
-        }
-
-        const showModal = () => setRegister(true);
-        const closeModal = () => setRegister(false);
+    const toggleFunction = () => {
+        setShow(!show)
+    }
 
 
-
-return (
-    <div>
-        <Form>
-            <FormGroup>
-                <Label for="loginPassword">
-                    Email
-                </Label>
-                <Input
-                    id="loginPassword"
-                    name="email"
-                    placeholder="email"
-                    type="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </FormGroup>
-            <FormGroup>
-                <Label for="loginPassword">
-                    Password
-                </Label>
-                <Input
-                    id="loginPassword"
-                    name="password"
-                    placeholder="password"
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </FormGroup>
-            <Button onClick={(e) => { loginSubmit(e) }}>
-                Submit
-            </Button>
-            <Button onClick={(e) => showModal}>
-            Register
-        </Button>
-            {/* <RegisterModal email={email} password={password} registerSubmit={registerSubmit} firstName={firstName} lastName={lastName} /> */}
-        </Form>
-        <Modal
-                show={register}
-                onHide={closeModal}
+    const ModalIsShowing = (e) => {
+        return (
+            <Modal
+                isOpen={show}
+                toggle={toggleFunction}
             >
-                <ModalHeader show={register} onHide={closeModal}>
+                <ModalHeader toggle={toggleFunction}>
                     Register
                 </ModalHeader>
                 <ModalBody>
-                    <Form>
-                        <FormGroup>
-                            <Label for="firstName">
-                                First Name
-                            </Label>
-                            <Input
-                                id="firstName"
-                                name="firstName"
-                                placeholder="First Name"
-                                type="text"
-                                onChange={(e) => setFirstName(e.target.value)}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="lastName">
-                                First Name
-                            </Label>
-                            <Input
-                                id="lastName"
-                                name="lastName"
-                                placeholder="Last Name"
-                                type="text"
-                                onChange={(e) => setLastName(e.target.value)}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="loginPassword">
-                                Email
-                            </Label>
-                            <Input
-                                id="loginPassword"
-                                name="email"
-                                placeholder="email"
-                                type="email"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="loginPassword">
-                                Password
-                            </Label>
-                            <Input
-                                id="loginPassword"
-                                name="password"
-                                placeholder="password"
-                                type="password"
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </FormGroup>
-                        <Button
-                            color="primary"
-                            onClick={registerSubmit}
-                        >
-                            Register
-                        </Button>
-                    </Form>
+                    <ReactForm toggleFunction={toggleFunction} show={show} updateLocalStorage={props.updateLocalStorage}/>
                 </ModalBody>
                 <ModalFooter>
-                    
-                    {' '}
-                    <Button onClick={closeModal}>
+                    <Button onClick={toggleFunction}>
                         Cancel
                     </Button>
                 </ModalFooter>
             </Modal>
-    </div>
-)
+        )
+    }
+
+
+    console.log(show)
+    return (
+        <div>
+            <Form>
+                <FormGroup>
+                    <Label for="loginPassword">
+                        Email
+                    </Label>
+                    <Input
+                        id="loginPassword"
+                        name="email"
+                        placeholder="email"
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="loginPassword">
+                        Password
+                    </Label>
+                    <Input
+                        id="loginPassword"
+                        name="password"
+                        placeholder="password"
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </FormGroup>
+                <Button onClick={(e) => { loginSubmit(e) }}>
+                    Submit
+                </Button>
+                <Button onClick={handleShow}>
+                    Register
+                </Button>
+            </Form>
+            {show === true ? <ModalIsShowing /> : <></>}
+        </div>
+    )
 }
 
 export default Auth;
