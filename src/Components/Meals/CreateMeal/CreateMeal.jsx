3b2b8722ed69f50a27ca79a8a579ce3325dd6dd2
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2021-12-14 13:20:56 -0800

    Set Names fx

--- src/Components/Meals/CreateMeal/CreateMeal.jsx
+++ src/Components/Meals/CreateMeal/CreateMeal.jsx
@@ -16,6 +16,7 @@ const CreateMeal = (props) => {
 
     const[foods, newFoods] = useState('')
     const[foodInput, newFoodInput] = useState('')
+    const[nutroArr, setNutroArr] = useState([])
 
     const fetchMeals = async () => {
         const apiKey = process.env.REACT_APP_API_KEY
@@ -32,9 +33,40 @@ const CreateMeal = (props) => {
             .catch(err => console.log(err))
             
     }
-    console.log(foods);
-
     
+    const setNames = (data) => {
+        for(let i=4; i < data.length; i--) {
+            let nutritionArr = data[i].foodNutrients;
+            // console.log(nutritionArr);
+            // let nutroArr = [];
+            for(let j=0; j < nutritionArr.length; j++) {
+                if(nutritionArr[j].nutrientId === 1003) {
+                    let proVal = nutritionArr[j].value
+                    newProtein(proVal)
+                    console.log("Protein:", proVal);
+                }
+                if(nutritionArr[j].nutrientId === 1005) {
+                    let carbVal = nutritionArr[j].value
+                    newCarbs(carbVal)
+                    console.log("Carb:", carbVal);
+                }
+                if(nutritionArr[j].nutrientId === 1004) {
+                    let fatVal = nutritionArr[j].value
+                    newFats(fatVal)
+                    console.log("Fat:", fatVal);
+                }
+                if(nutritionArr[j].nutrientId === 1008) {
+                    let cal = nutritionArr[j].value
+                    newKCal(cal)
+                    console.log("KCal", cal);
+                }
+                // nutroArr.push(proVal, carbVal, fatVal, cal)
+                // console.log(nutroArr);
+            }
+        }
+    }
+    console.log(setNames(foods))
+    // console.log(protein);
     
     useEffect(() => {
         
